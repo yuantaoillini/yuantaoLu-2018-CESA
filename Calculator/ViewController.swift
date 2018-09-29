8295b56c8c20a101db9e957794954148099491f5
Yuantao Lu <yuantao3@illinois.edu>
2018-09-29 15:20:37 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 陆元涛 on 2018/9/29.
+//  Copyright © 2018 Yuantao Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
